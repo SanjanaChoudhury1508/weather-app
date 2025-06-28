7ffc3a7201ecde1af8202c32ded63e66580bbbfa
Sanjana Choudhury <sanjana7choudhury@gmail.com>
2025-06-28 16:49:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const apiKey = "43841a1dc09fee2567c0aa1d49ea1148"; 
+
+function getWeather() {
+  const city = document.getElementById("cityInput").value;
+  if (!city) return;
+
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("temp").textContent = data.main.temp + "°";
+      document.getElementById("city").textContent = data.name;
+      document.getElementById("desc").textContent = data.weather[0].description;
+
+      const now = new Date();
+      const timeStr = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+      const dayStr = now.toLocaleDateString([], { weekday: 'long' });
+      document.getElementById("time").textContent = `${timeStr} ${dayStr}`;
+    })
+    .catch((err) => {
+      alert("City not found!");
+    });
+}
